55a356d1abdee87bb15486cc3644ee4b1c483144
EmmieKing <37819033+EmmieKing@users.noreply.github.com>
2018-08-28 13:28:01 -0400

    Fixed flake8 issues

--- adam/adam_config.py
+++ adam/adam_config.py
@@ -8,7 +8,7 @@ import os
 
 
 class setPaths(object):
-    def initPaths(home = None):
+    def initPaths(home=None):
 
         # Initialize ADAM_home directory path
         pathExists = os.path.exists(home)
@@ -24,25 +24,32 @@ class setPaths(object):
         OS = "/"
         config_file = home + OS + 'config' + OS + 'adam_config.json'
         config_template_file = home + OS + 'config' + OS +\
-                                'adam_config_template.json'
+                               'adam_config_template.json'
 
         try:
             f = open(config_file)
             f.close()
             file_to_open = config_file
-        except:
-            print("adam_config.json NOT FOUND - loading adam_config_template.json")
+        except FileNotFoundError:
+            print("adam_config.json NOT FOUND -\
+                 loading adam_config_template.json")
             file_to_open = config_template_file
 
         # Load ADAM_config.json file to set standard paths wrt ADAM_home
         with open(file_to_open, 'r') as f:
             raw_config = json.load(f)
-            adam_path = home + OS + raw_config['adam_config']['adam_package_path']
-            data_path = home + OS + raw_config['adam_config']['data_path']
-            env_template_path =  home + OS + raw_config['adam_config']['environment_template_file']
-            env_config_path = home + OS + raw_config['adam_config']['environment_config_file']
-            ephem_path = home + OS + raw_config['adam_config']['ephem_path']
-            MY_functions_path = home + OS + raw_config['MY_config']['MY_functions_path']
+            adam_path = home + OS +\
+                        raw_config['adam_config']['adam_package_path']
+            data_path = home + OS +\
+                        raw_config['adam_config']['data_path']
+            env_template_path = home + OS +\
+                        raw_config['adam_config']['environment_template_file']
+            env_config_path = home + OS +\
+                        raw_config['adam_config']['environment_config_file']
+            ephem_path = home + OS +\
+                        raw_config['adam_config']['ephem_path']
+            MY_functions_path = home + OS +\
+                        raw_config['MY_config']['MY_functions_path']
 
         return (adam_path, data_path, env_template_path,
                 env_config_path, ephem_path, MY_functions_path)
