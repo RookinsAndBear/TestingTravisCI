590056d53fbce528a6c20710d6c6c89a8a3ec8f4
EmmieKing <37819033+EmmieKing@users.noreply.github.com>
2018-10-04 14:17:44 -0400

    Update notebook_test.py

--- adam_home/tests/notebook_test.py
+++ adam_home/tests/notebook_test.py
@@ -29,22 +29,19 @@ def _process_notebook(path):
     
     # in_file = '/home/travis/build/RookinsAndBear/TestingTravisCI/adam_home/demos/Orbit_Period_Uncertainty_Trending_demo.ipynb'
     dirname, in_file = os.path.split(path)
-    jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60\ 
-                  --output jnb in_file
     
     # os.chdir(dirname)
     # convert *.ipynb from jupyter notebook to py notebook
-    #with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-    #    args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-    #            "--ExecutePreprocessor.timeout=120",
-    #            "--output", fout.name, path]
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=120",
+                "--output", fout.name, path]
         # submodule allows you to spawn new processes, connect to their input/
         # output/error pipes, and obtain their return codes.
-    subprocess.check_call(args)
+        subprocess.check_call(args)
         # seek() sets the file's current position.
-    #    fout.seek(0)
-       
-    nb = nbformat.read(in_file, nbformat.current_nbformat)
+        fout.seek(0)
+        nb = nbformat.read(fout, nbformat.current_nbformat)
         
     errors = [output for cell in nb.cells if "outputs" in cell
               for output in cell["outputs"]\
@@ -56,4 +53,5 @@ def test():
     _exec_notebook('/home/travis/build/RookinsAndBear/TestingTravisCI/adam_home/demos/example.ipynb')
     _exec_notebook('/home/travis/build/RookinsAndBear/TestingTravisCI/adam_home/demos/Orbit_Period_Uncertainty_Trending_demo.ipynb') 
     nb, errors = _process_notebook('/home/travis/build/RookinsAndBear/TestingTravisCI/adam_home/demos/Orbit_Period_Uncertainty_Trending_demo.ipynb')
+    # assert that errors is empty, otherwise fail
     assert errors == []
