9fe3c88d40df5329aa98ba4815d8e23435ea0096
EmmieKing <Emmie.King@gmail.com>
2019-08-09 11:51:44 -0400

    revert to original code

--- adam_home/tests/integration_tests/anonymous_test.py
+++ adam_home/tests/integration_tests/anonymous_test.py
@@ -23,13 +23,13 @@ class AnonymousTest(unittest.TestCase):
         testdir = os.getcwd()
         print("Test directory (go up 1 level): ", testdir)
         #Test directory (go up 1 level):  /home/travis/build/RookinsAndBear/TestingTravisCI
-        print(sys.path)
-        #self.config = ConfigManager(os.getcwd() + '/test_adam_config.json').get_config()
-        config_env_token = ConfigManager(os.getcwd() + '/test_adam_config.json').get_config()
-        print(config_env_token)
-        #self.config.set_token("")
-        #self.service = Service(self.config)
-        #self.assertTrue(self.service.setup())
+        #print(sys.path)
+        self.config = ConfigManager(os.getcwd() + '/test_adam_config.json').get_config()
+        #config_env_token = ConfigManager(os.getcwd() + '/test_adam_config.json').get_config()
+        #print(config_env_token)
+        self.config.set_token("")
+        self.service = Service(self.config)
+        self.assertTrue(self.service.setup())
 
 
     def tearDown(self):
