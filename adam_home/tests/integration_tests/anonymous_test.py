ebd11d57e70da7d01b5d647711b47960da4827bc
EmmieKing <Emmie.King@gmail.com>
2019-08-09 12:03:59 -0400

    invoke read_config method

--- adam_home/tests/integration_tests/anonymous_test.py
+++ adam_home/tests/integration_tests/anonymous_test.py
@@ -24,8 +24,10 @@ class AnonymousTest(unittest.TestCase):
         print("Test directory (go up 1 level): ", testdir)
         #Test directory (go up 1 level):  /home/travis/build/RookinsAndBear/TestingTravisCI
         #print(sys.path)
-        #self.config = ConfigManager(os.getcwd() + '/test_adam_config.json').get_config()
-        config_env_token = ConfigManager(os.getcwd() + '/test_adam_config.json').get_config()
+        self.config = ConfigManager(os.getcwd() + '/test_adam_config.json').get_config()
+        #config_env_token = ConfigManager(os.getcwd() + '/test_adam_config.json').get_config()
+        #print(config_env_token)
+        config_env_token = ConfigManager(os.getcwd() + '/test_adam_config.json').read_config()
         print(config_env_token)
         self.config.set_token("")
         self.service = Service(self.config)
