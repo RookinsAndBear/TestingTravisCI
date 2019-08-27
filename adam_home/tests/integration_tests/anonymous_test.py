9310c3191d50caab7cf5d2a85eb635b873fa7e68
RookinsAndBear <emmie.king@gmail.com>
2019-08-27 14:53:17 -0400

    add string test for home/travis/build

--- adam_home/tests/integration_tests/anonymous_test.py
+++ adam_home/tests/integration_tests/anonymous_test.py
@@ -20,6 +20,11 @@ class AnonymousTest(unittest.TestCase):
         print("Current working dir: ", cwd) 
         cwd_str = str(cwd).split("/")
         print(cwd_str)
+        matching = [s for s in cwd_str if "travis" in s]
+        print(matching)
+        # use == or is?
+        if cwd_str[0] == "home" and cwd_str[1] == "travis" and cwd_str[2] == "build":
+            print("home/travis/build found in root dir")
         #Current working dir:  /home/travis/build/RookinsAndBear/TestingTravisCI/adam_home
         os.chdir("..")
         testdir = os.getcwd()
